884a32a9d690d759ca0ff5c29e7e7e8f8f53bb87
kyounghwan <osc9245@naver.com>
2020-05-04 23:27:33 +0900

    --all.sh master

--- docs/기타/index.js
+++ docs/기타/index.js
@@ -1,6 +1,13 @@
 module.exports = {
   '/기타/': [
     '',
+    {
+      collapsable: true,
+      title: 'CSS',
+      children: [
+        '/기타/CSS/dup-area-event/',
+      ]
+    },
     '/기타/prettierrc/',
     '/기타/react-native-webview/',
   ]
