127c716103c6c815822955383ed3e770e20ed9d2
kyounghwan <osc9245@naver.com>
2020-05-23 22:35:49 +0900

    feat: google search console 소유권 확인

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,25 +1,26 @@
 // const routes = ['Vue', '개발일기', 'Job', 'About'];
-const routes = ['Vue', 'JS', 'TS', '기타', 'About', '개발일기'];
+const routes = ["Vue", "JS", "TS", "기타", "About", "개발일기"];
 
 const createSidebar = () => {
-  const sidebar = {}
+  const sidebar = {};
   for (const route of routes) {
-    Object.assign(sidebar, require('../' + route))
+    Object.assign(sidebar, require("../" + route));
   }
-  return sidebar
+  return sidebar;
 };
 
 module.exports = {
   title: "기억보다 기록을",
   description: "vuepress 기반 기술 블로그입니다.",
+  "google-site-verification": "J993cuJJ-yFppfJKY8rMx3SzsN-3ZMBEM4uY7Jj075A",
   themeConfig: {
     nav: [
-      { text: 'Vue', link:"/Vue/"},
-      { text: 'JS', link:"/JS/"},
-      { text: 'TS', link:"/TS/"},
+      { text: "Vue", link: "/Vue/" },
+      { text: "JS", link: "/JS/" },
+      { text: "TS", link: "/TS/" },
       { text: "기타", link: "/기타/" },
-      { text: '개발일기', link:"/개발일기/"},
-      { text: 'About', link:"/About/"},
+      { text: "개발일기", link: "/개발일기/" },
+      { text: "About", link: "/About/" },
       { text: "Job", link: "/Job/" },
       { text: "Github", link: "https://github.com/Kyounghwan01" },
       {
@@ -30,14 +31,17 @@ module.exports = {
             text: "마타드림 앱 깃헙 주소",
             link: "https://github.com/Kyounghwan01/mata-dream-app"
           },
-          { text: "매출분석 - web(vue + aws-lambda)", link: "https://sales-statistics.netlify.com/" },
+          {
+            text: "매출분석 - web(vue + aws-lambda)",
+            link: "https://sales-statistics.netlify.com/"
+          }
         ]
       }
     ],
     sidebar: createSidebar()
   },
   base: "/blog/",
-  plugins: ['@vuepress/back-to-top'],
+  plugins: ["@vuepress/back-to-top"]
   // markdown: {
   //   lineNumbers: true
   // }
