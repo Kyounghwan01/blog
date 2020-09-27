79db62e57fea32495bb26fd97283ad078c7b61cd
khnoh <noh5524@gmail.com>
2020-09-27 22:30:35 +0900

    fix: 아코디언 형식 게시글 등록 자동화 버그 수정

--- docs/error-report/index.js
+++ docs/error-report/index.js
@@ -15,28 +15,28 @@ function getFiles(dir) {
     if (fs.statSync(`${dir}/${file}`).isDirectory()) {
       return getFiles(`${dir}/${file}`);
     } else {
-      if (dir.split("/").length === 3) {
-        once.push(`${dir.slice(1)}/`);
-      } else if (dir.split("/").length === 4) {
+      if (dir.split("/").length === 4) {
+        once.push(`${dir.slice(6)}/`);
+      } else if (dir.split("/").length === 5) {
         let check = false;
         nesting.map(el => {
-          if (el.title === dir.split("/")[dir.split("/").length - 2]) {
+          if (el.title === dir.split("/")[dir.split("/").length - 3]) {
             check = true;
-            el.children.push(`${dir.slice(1)}/`);
+            el.children.push(`${dir.slice(6)}/`);
           }
         });
         if (!check) {
           nesting.push({
             collapsable: true,
-            title: dir.split("/")[dir.split("/").length - 2],
-            children: [`${dir.slice(1)}/`]
+            title: dir.split("/")[dir.split("/").length - 3],
+            children: [`${dir.slice(6)}/`]
           });
         }
       }
     }
   });
 }
-getFiles(`./${dirName}`);
+getFiles(`./docs/${dirName}`);
 const res = listsNesting.concat(nesting).concat(once);
 module.exports = {
   [`/${dirName}/`]: res
