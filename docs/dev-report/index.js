03a458e354a9703c52f52314b54a457b1641c7c5
khnoh <noh5524@gmail.com>
2020-09-27 20:49:31 +0900

    feat: 게시글 module 등록 자동화

--- docs/dev-report/index.js
+++ docs/dev-report/index.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const dirName = "dev-report";
 
 const lists = [""];
-fs.readdir(`./${dirName}`, function(error, fileList) {
+fs.readdir(`./docs/${dirName}`, function(error, fileList) {
   for (let list of fileList) {
     if (list.indexOf(".") === -1) {
       lists.push(`${list}/`);
