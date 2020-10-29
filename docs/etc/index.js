dcb37399722b11d990cef90ef71d20c6cfb83b8f
khnoh <noh5524@gmail.com>
2020-10-29 20:45:04 +0900

    [#11] feat: .env로 dev / build 환경 구분 자동화

--- docs/etc/index.js
+++ docs/etc/index.js
@@ -1,3 +1,7 @@
+const envValue = process.env.NODE_ENV
+  ? { "43": 4, "61": 6, "54": 5 }
+  : { "43": 3, "61": 1, "54": 4 };
+
 const fs = require("fs");
 const dirName = "etc";
 
@@ -15,21 +19,21 @@ function getFiles(dir) {
     if (fs.statSync(`${dir}/${file}`).isDirectory()) {
       return getFiles(`${dir}/${file}`);
     } else {
-      if (dir.split("/").length === process.env.NODE_ENV ? 4 : 3) {
-        once.push(`${dir.slice(process.env.NODE_ENV ? 6 : 1)}/`);
-      } else if (dir.split("/").length === process.env.NODE_ENV ? 5 : 4) {
+      if (dir.split("/").length === envValue[43]) {
+        once.push(`${dir.slice(envValue[61])}/`);
+      } else if (dir.split("/").length === envValue[54]) {
         let check = false;
         nesting.map(el => {
           if (el.title === dir.split("/")[dir.split("/").length - 2]) {
             check = true;
-            el.children.push(`${dir.slice(process.env.NODE_ENV ? 6 : 1)}/`);
+            el.children.push(`${dir.slice(envValue[61])}/`);
           }
         });
         if (!check) {
           nesting.push({
             collapsable: true,
             title: dir.split("/")[dir.split("/").length - 2],
-            children: [`${dir.slice(process.env.NODE_ENV ? 6 : 1)}/`]
+            children: [`${dir.slice(envValue[61])}/`]
           });
         }
       }
