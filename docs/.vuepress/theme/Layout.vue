1143e8457fbed89e932794eb6980e96342628043
khnoh <noh5524@gmail.com>
2021-03-24 21:41:35 +0900

    test: redirect new blog

--- docs/.vuepress/theme/Layout.vue
+++ docs/.vuepress/theme/Layout.vue
@@ -18,10 +18,6 @@ export default {
   components: {
     ParentLayout,
     Disqus
-  },
-  beforeMount() {
-    const location = window.location.href.split("blog")[1];
-    window.location.href = `https://kyounghwan01.github.io${location}`;
   }
 };
 </script>
