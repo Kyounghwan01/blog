50357c5bc0e1abe799d4206904ea9a0581f7a9a2
Kyounghwan01 <noh5524@gmail.com>
2022-06-01 22:31:29 +0900

    feat: google analytice ua -> ga4로 변경

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -22,6 +22,20 @@ module.exports = {
     [
       "script",
       {
+        async: true,
+        src: "https://www.googletagmanager.com/gtag/js?id=G-GK96B5Q500"
+      }
+    ],
+    [
+      "script",
+      {},
+      [
+        "window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-GK96B5Q500');"
+      ]
+    ],
+    [
+      "script",
+      {
         "data-ad-client": "ca-pub-6192551181326997",
         async: true,
         src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
@@ -50,24 +64,6 @@ module.exports = {
             text: "블로그 건의하기",
             link: "https://github.com/Kyounghwan01/blog/issues/20"
           }
-          // {
-          //   text: "toy-project",
-          //   items: [
-          //     {
-          //       text: "트렐로 클론 (svelte)",
-          //       link: "https://svelte-trello.netlify.app/"
-          //     },
-          //     {
-          //       text: "매출분석 (vue + aws-lambda)",
-          //       link: "https://sales-statistics.netlify.com/"
-          //     },
-          //     {
-          //       text: "롤전적조회 (react)",
-          //       link: "https://khgg.netlify.app/"
-          //     }
-          //   ]
-          // }
-          // { text: "It's my seat - web", link: "https://www.knowgari.com/" }
         ]
       }
     ],
@@ -79,16 +75,16 @@ module.exports = {
     // [
     //   "vuepress-plugin-google-adsense",
     //   {
-    //     google_ad_client: "ca-pub-6192551181326997", // ca-pub-0000000000000000
+    //     google_ad_client: "ca-pub-6192551181326997",
     //     enable_page_level_ads: true
     //   }
     // ],
-    [
-      "@vuepress/google-analytics",
-      {
-        ga: "UA-172169202-1" // UA-00000000-0
-      }
-    ],
+    // [
+    //   "@vuepress/google-analytics",
+    //   {
+    //     ga: "UA-172169202-1" // UA-00000000-0
+    //   }
+    // ],
     ["@vuepress/back-to-top"],
     ["vuepress-plugin-code-copy"],
     ["sitemap", { hostname: "https://kyounghwan01.github.io/blog/" }],
